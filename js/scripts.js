9b10791533a54e3ca200dedc62aaa7831aae2909
RosarioRu <mariarruval@gmail.com>
2022-05-05 09:47:25 -0700

    Draft code for PizzaPie() that passes the first test.

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+//Business Logic-------------------------------------------
+
+//Constructor function to make pizza pies
+
+function PizzaPie(size, toppings) {
+  this.size = (size);
+  this.toppings = [toppings];
+}
+
+
+
+
+//Prototype function/s available to all instances of pizza pies
+
+
+
+
+
+
+
+
+//UI Logic--------------------------------------
+
